bfba1a858306d15a235feafaeefe716ea424354f
Betty Lin <bettyhappylin@gmail.com>
2017-06-25 13:20:22 -0400

    added instruction text

--- app/components/Instructions.jsx
+++ app/components/Instructions.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const Instructions = props => {
-
+  return (
+    <div>
+      <h1>How to Play Witches Brew</h1>
+      <h3>A party game for bad witches</h3>
+      <p>Join a coven (2-4 players) and work together to add ingredients to the cauldron. Finish the potion to cast a spell onto the evil wizard before time runs out!</p>
+    </div>
+  )
 }
 
 export default Instructions
