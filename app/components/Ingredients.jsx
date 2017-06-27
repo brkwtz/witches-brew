a2c71c97e261ac07ba850fadc7f59fbc093773e8
Betty Lin <bettyhappylin@gmail.com>
2017-06-27 16:41:52 -0400

    working countdown timer for when commands expire

--- app/components/Ingredients.jsx
+++ app/components/Ingredients.jsx
@@ -13,72 +13,8 @@ export class Ingredients extends React.Component {
       win: this.props.win,
       levelEnd: this.props.levelEnd
     }
+
     this.selectIngredient = this.selectIngredient.bind(this)
-    this.tick = this.tick.bind(this)
-  }
-
-  componentDidMount() {
-    // reliant on the level
-    let sec = 0
-    if (this.props.level <= 2) {
-      sec = 600
-    } else { sec = 400 }
-    const timer = setInterval(this.tick, sec)
-    this.setState({timer})
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.state.timer)
-  }
-
-  tick() {
-    let numLoops = this.props.commands.length + 1
-    if (numLoops >= 0) {
-      this.setState({
-        counter: this.state.counter + 10
-      })
-
-      const purpleBar = document.getElementById('purpleBar')
-      purpleBar.style.width = this.state.counter + '%'
-
-      if (this.state.counter >= 100) {
-        // reset the interval, and the bar style, and the state counter
-        clearInterval(this.state.timer)
-
-        this.setState({
-          counter: 0
-        })
-
-        purpleBar.style.width = 0 + '%'
-
-        console.log('now expired')
-
-        // get a new command
-        this.props.commandExpired(this.props.commands)
-
-        // if there's another command, restart the timer
-        if (numLoops >= 0) {
-          let sec = 0
-
-          if (this.props.level <= 2) {
-            sec = 600
-          } else {
-            sec = 400
-          }
-
-          const timer = setInterval(this.tick, sec)
-          this.setState({timer: timer})
-          console.log(this.props.commands)
-          numLoops--
-        }
-
-        if (numLoops === 0) {
-          console.log('commands go down to 0!!!!!!')
-          clearInterval(this.state.timer)
-          this.setState({timer: null})
-        }
-      }
-    }
   }
 
   componentWillReceiveProps(newProps) {
@@ -89,14 +25,6 @@ export class Ingredients extends React.Component {
 
   selectIngredient = (ingredient) => () => {
     this.props.addIngredient(ingredient)
-    let sec = 0
-    if (this.props.level <= 2) {
-      sec = 600
-    } else {
-      sec = 400
-    }
-    const timer = setInterval(this.tick, sec)
-    this.setState({timer})
   }
 
   render() {
@@ -106,8 +34,6 @@ export class Ingredients extends React.Component {
         <h1>{this.state.currentCommand}</h1>
 
         <hr />
-        <div id="greyBar">
-        <div id="purpleBar"></div></div>
         <h3>Ingredients</h3>
         {
           ingredients && ingredients.map((ingredient, idx) => (
@@ -116,7 +42,6 @@ export class Ingredients extends React.Component {
               </div>
             ))
         }
-        <div> Loading {this.state.counter}</div>
       </div>
     )
   }
--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import firebase from 'APP/fire'
 import {connect} from 'react-redux'
+import raf from 'raf'
 
 import {commandExpired} from './reducers'
 
@@ -8,15 +9,49 @@ export class Timer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      timerPosition: 0,
-      isTicking: false
+      time: 7000, // timer start in milliseconds
+      cycle: 0
+    }
+    this.tick = this.tick.bind(this)
+    this.stopTime = Date.now() + this.state.time
+  }
+
+  componentDidMount() {
+    this.time = setInterval(this.tick, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.time)
+  }
+
+  tick() {
+    let now = Date.now()
+    if (this.state.cycle === 4) {
+      clearInterval(this.time)
+    } else if (this.stopTime - now <= 0) {
+      console.log('PROOOOPS', this.props)
+      this.setState({time: 0})
+      clearInterval(this.time)
+
+      this.props.commandExpired(this.props.currentPlayer.uid)
+      this.setState({
+        time: 7000,
+        cycle: this.state.cycle + 1
+      })
+      this.stopTime = Date.now() + this.state.time
+      this.time = setInterval(this.tick, 1000)
+    } else {
+      this.setState({
+        time: this.stopTime - now
+      })
     }
   }
 
   render() {
+    const time = this.state.time / 1000
     return (
       <div>
-      <h1>{this.state.timerPosition}</h1>
+      <span style={{color: 'red'}}>{Math.ceil(time)}</span>
       </div>
     )
   }
