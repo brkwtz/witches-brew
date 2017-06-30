24762cc385ed601d2901225e09724a4235d492b8
Jin Lin <jinlin314@hotmail.com>
2017-06-30 17:24:33 -0400

    delete room after room is empty, restart game if a player left

--- functions/index.js
+++ functions/index.js
@@ -11,17 +11,22 @@ const functions = require('firebase-functions')
 // uppercase version of the message to /messages/:pushId/uppercase
 exports.sendPresenceActions = functions.database.ref('/gamerooms/{gameId}/roster/{userId}')
   .onWrite(event => {
-    // Grab the current value of what was written to the Realtime Database.
+    const room = event.data.ref.parent.parent
+    const actions = room.child('actions')
+    const roster = room.child('roster')
+
     const isThere = event.data.val()
     if (isThere) {
-      return event.data.ref.parent.parent.child('actions').push({
+      return actions.push({
         type: 'PLAYER_JOIN',
         player: {uid: event.params.userId}
       })
     }
 
-    return event.data.ref.parent.parent.child('actions').push({
-      type: 'PLAYER_LEAVE',
-      player: {uid: event.params.userId}
-    })
+    return roster.once('value').then(roster => roster.val()
+        ? actions.push({
+          type: 'PLAYER_LEAVE',
+          player: {uid: event.params.userId}
+        })
+        : room.remove())
   })
