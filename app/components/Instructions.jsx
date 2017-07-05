3e0be380aa55bfad170270c081be86e3b8be06e4
Jin Lin <jinlin314@hotmail.com>
2017-07-05 15:40:32 -0400

    text instruction taken out, instruction on scroll added

--- app/components/Instructions.jsx
+++ app/components/Instructions.jsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router'
 
 const Instructions = props => {
   return (
-    <div>
-      <h1>How to Play Witches Brew</h1>
-      <h3>A party game for bad witches</h3>
-      <p>Join a coven (2-4 players) and work together to add ingredients to the cauldron. Finish the potion to cast a spell onto the evil wizard before time runs out!</p>
+    <div className="instruction">
+      {/*<h1>How to Play Witches Brew</h1>*/}
+      {/*<h3>A party game for bad witches</h3>*/}
+      {/*<p>Join a coven (2-4 players) and work together to add ingredients to the cauldron. Finish the potion to cast a spell onto the evil wizard before time runs out!</p>*/}
+      <img src="/gifs/instruction.gif" />
       <Link to='/coven'>back</Link>
     </div>
   )
