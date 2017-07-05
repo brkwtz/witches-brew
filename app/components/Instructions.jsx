3fa4588caf4aec84db31eec39eec98fef7e840fd
Jin Lin <jinlin314@hotmail.com>
2017-07-05 16:32:06 -0400

    restyle the scroll, back image button added

--- app/components/Instructions.jsx
+++ app/components/Instructions.jsx
@@ -3,12 +3,9 @@ import { Link } from 'react-router'
 
 const Instructions = props => {
   return (
-    <div className="instruction">
-      {/*<h1>How to Play Witches Brew</h1>*/}
-      {/*<h3>A party game for bad witches</h3>*/}
-      {/*<p>Join a coven (2-4 players) and work together to add ingredients to the cauldron. Finish the potion to cast a spell onto the evil wizard before time runs out!</p>*/}
-      <img src="/gifs/instruction.gif" />
-      <Link to='/coven'>back</Link>
+    <div className="center">
+      <img className="instruction center" src="/gifs/scroll.gif" />
+      <p><Link to='/coven'><img src="/gifs/backButton.gif"/></Link></p>
     </div>
   )
 }
